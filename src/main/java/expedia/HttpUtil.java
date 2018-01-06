06170c3ff6ccfd84018bc71c8086daee30933a2a
faisal khader <faisal.khader@Jt.JtGroup>
2018-01-06 23:50:28 +0200

    Final Version

--- src/main/java/expedia/HttpUtil.java
+++ src/main/java/expedia/HttpUtil.java
@@ -8,12 +8,9 @@ import org.javalite.http.Http;
 
 public class HttpUtil {
 
-	//private String url ="https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel";
-	//private HashMap<String,String> params = new HashMap<String,String>();
 
 	public static String doGetRequest(String url,Map<String,String> params){
 
-		//HashMap<String,String> paramValue = getParams();
 		for (String key : params.keySet()) {
 			if(!"".equals((params.get(key)))){
 				url = url.concat("&" + key +"="+ params.get(key)); 
@@ -24,21 +21,6 @@ public class HttpUtil {
 		return get.text();
 	}
 
-//	public static void main(String[] args) {
-//		HttpUtil test  = new HttpUtil();
-//		String s = test.getSearchResult();
-//		System.out.println("Testing URL:" + s);
-//			
-//	}
-	
-//	public HashMap<String,String> getParams() {		
-//		params.put("destinationName", "Amman");
-//		params.put("lengthOfStay", "5");		
-//		return params;
-//	}
-//
-//	public void setParams(HashMap<String,String> params) {
-//		this.params = params;
-//	}
+
 
 }
